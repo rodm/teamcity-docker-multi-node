c899b0591aa19ca4540a8d0226fa00a6be611987
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2022-06-19 18:50:47 +0100

    Add example TeamCity plugin

---
+++ plugin/build.gradle.kts
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 Rod MacKenzie
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    id ("java")
+    id ("com.github.rodm.teamcity-server")
+}
+
+group = "com.github.rodm.teamcity"
+version = "1.0-SNAPSHOT"
+
+val vendorName by extra("rodm")
+val teamcityVersion by extra("2020.1")
+
+teamcity {
+    version = teamcityVersion
+
+    server {
+        archiveName = "teamcity-plugin.zip"
+        descriptor {
+            name = project.name
+            displayName = project.name
+            description = "Example TeamCity Server Plugin"
+            version = project.version as String
+            vendorName = extra["vendorName"] as String
+            vendorUrl = "https://github.com/rodm"
+            email = "rod.n.mackenzie@gmail.com"
+            useSeparateClassloader = true
+
+            parameters {
+                parameter ("param", "example server value")
+            }
+        }
+    }
+}
---
+++ plugin/src/main/java/example/teamcity/ExampleServerPlugin.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022 Rod MacKenzie
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.teamcity;
+
+import jetbrains.buildServer.serverSide.BuildServerAdapter;
+import jetbrains.buildServer.serverSide.SBuildServer;
+import jetbrains.buildServer.web.openapi.PluginDescriptor;
+import org.apache.log4j.Logger;
+
+public class ExampleServerPlugin extends BuildServerAdapter {
+
+    private static Logger logger = Logger.getLogger("jetbrains.buildServer.SERVER");
+
+    private final SBuildServer server;
+
+    private final PluginDescriptor descriptor;
+
+    public ExampleServerPlugin(SBuildServer server, PluginDescriptor descriptor) {
+        this.server = server;
+        this.descriptor = descriptor;
+    }
+
+    public void register() {
+        server.addListener(this);
+        String name = getClass().getSimpleName();
+        logger.info(String.format("%s: Plugin name: %s, version: %s", name, descriptor.getPluginName(), descriptor.getPluginVersion()));
+        logger.info(String.format("%s: Plugin parameter: param=%s", name, descriptor.getParameterValue("param")));
+    }
+}
