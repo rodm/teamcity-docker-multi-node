1ec472c5ebbeb4f2978f30a822754c8e4d6bc50d
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2022-08-04 15:17:20 +0100

    Update Gradle TeamCity plugin

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 }
 
 dependencies {
-    implementation ("io.github.rodm:gradle-teamcity-plugin:1.5-beta-2a")
+    implementation ("io.github.rodm:gradle-teamcity-plugin:1.5-beta-3")
 }
 
 gradlePlugin {
